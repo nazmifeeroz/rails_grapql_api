69fd540771de5403f82e185affb2085a22964138
Nazmi <nazmifeeroz@hotmail.com>
2018-10-13 16:24:49 +0800

    graphql spec wip

--- app/graphql/todos_graphql_api_schema.rb
+++ app/graphql/todos_graphql_api_schema.rb
@@ -1,4 +1,10 @@
-class TodosGraphqlApiSchema < GraphQL::Schema
+# frozen_string_literal: true
+
+# class TodosGraphqlApiSchema < GraphQL::Schema
+#   mutation(Types::MutationType)
+#   query(Types::QueryType)
+# end
+class TodosGraphqlApiSchema < GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
---
+++ app/graphql/types/todo_list.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# module Types
+#   TodoListType = GraphQL::ObjectType.define do
+#     name 'TodoListType'
+#     description 'The Todo List type'
+
+#     field :id, !types.ID
+#     field :title, !types.String
+#   end
+# end
+ class TodoListType < GraphQL::ObjectType.define do
+    name 'TodoListType'
+    description 'The Todo List type'
+
+    field :id, !types.ID
+    field :title, !types.String
+    field :items, types[Types::ItemType] do
+      resolve ->(obj, _args, _ctx) { obj.items }
+    end
+  end
